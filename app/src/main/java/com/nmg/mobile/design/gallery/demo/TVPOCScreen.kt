8f286194ded44a65de06c35bc66d98ff810a9af1
James Kong <104340270+jameskongchief@users.noreply.github.com>
2023-10-07 15:35:50 +0800

    feature/POC hybrid player

--- app/src/main/java/com/nmg/mobile/design/gallery/demo/TVPOCScreen.kt
+++ app/src/main/java/com/nmg/mobile/design/gallery/demo/TVPOCScreen.kt
@@ -1,7 +1,10 @@
 package com.nmg.mobile.design.gallery.demo
 
 import android.net.Uri
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.nmg.mobile.design.widgets.videoplayer.VideoPlayer
 import com.nmg.mobile.design.widgets.ytplayer.YTPlayerView
 
@@ -12,10 +15,9 @@ fun TVPOCScreen() {
     }
 }
 
-
 @Composable
 fun TVPOCVimeoScreen() {
     TVPOC("https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8") { url, lifecycleOwner ->
-        VideoPlayer(Uri.parse(url))
+        VideoPlayer(Uri.parse(url), modifier = Modifier.fillMaxWidth().fillMaxHeight(0.33f))
     }
 }
