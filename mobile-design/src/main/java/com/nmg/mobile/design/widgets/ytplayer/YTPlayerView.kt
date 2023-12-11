18d9d5a39f6b46deee2897b728d34dddddda7581
dontan <390032295@qq.com>
2023-12-11 23:14:16 +0800

    - [Dev - [ED_AOS] Build the video section on NMG app](https://app.clickup.com/t/865czme9k)

--- mobile-design/src/main/java/com/nmg/mobile/design/widgets/ytplayer/YTPlayerView.kt
+++ mobile-design/src/main/java/com/nmg/mobile/design/widgets/ytplayer/YTPlayerView.kt
@@ -18,7 +18,7 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.loadOrC
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 @Composable
-fun YTPlayerView(lifecycleOwner: LifecycleOwner, videoId: String, modifier: Modifier = Modifier,) {
+fun YTPlayerView(lifecycleOwner: LifecycleOwner, videoId: String, modifier: Modifier = Modifier) {
     var isFullscreen by remember { mutableStateOf(false) }
     var youTubePlayerView by remember { mutableStateOf<YouTubePlayerView?>(null) }
     var lifecycleEvent by remember { mutableStateOf(Lifecycle.Event.ON_ANY) }
